44a6eea594cd39c294ddc5db1e454fde1d786590
Osvaldo Bastos Teixeira Junior <115334336+odesenvolvedor2022@users.noreply.github.com>
2022-11-03 21:26:07 -0300

    sistema de troca de tema finalizado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 funcionamento = "Rodando JavaScript...";
 
+function setLogo(){
+
+}
+
 function createGame(player1, player2, hour){
   return `
   <li>
@@ -18,27 +22,46 @@ function createGame(player1, player2, hour){
   `;
 };
 
+let delay = -0.25
 function createCard(date, day, games) {
+  delay += 0.25
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
     <h2>${date} <span>${day}</span></h2>
     <ul>${games}</ul>
   </div>
   `
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-  <img src="assets/logo.svg" alt="logo da copa 2022" />
-</header>
-<main class="cards">
-  ${createCard("20/11","domingo",
-    createGame("catar","equador","13:00")
-  )}
-  ${createCard("21/11","segunda", 
-    createGame("inglaterra","iran","10:00")+
-    createGame("senegal","holanda","13:00")+
-    createGame("estados unidos","gales","16:00")
-  )}
-</main>
-`
+document.querySelector(".cards").innerHTML = 
+createCard("20/11","domingo",
+  createGame("catar","equador","13:00")
+)+
+createCard("21/11","segunda", 
+  createGame("inglaterra","iran","10:00")+
+  createGame("senegal","holanda","13:00")+
+  createGame("estados unidos","gales","16:00")
+);
+
+let body = document.querySelector("body");
+let classBody = body.getAttribute("class");
+let logo = document.querySelector(".logo");
+
+let setCor = document.querySelectorAll(".cor_tema");
+
+function mostrarCor(cor){
+  logo.setAttribute("src", `assets/logo/logo_${cor}.svg`);
+  trocarCorBody(cor);
+
+  for(let i = 0; i < setCor.length; i++){
+    setCor[i].classList.remove("oculto");
+  }
+  document.getElementById(cor).classList.add("oculto");
+}
+
+function trocarCorBody(classe){
+  body.classList.remove(classBody);
+  body.classList.add(classe);
+  classBody = classe
+}
+
